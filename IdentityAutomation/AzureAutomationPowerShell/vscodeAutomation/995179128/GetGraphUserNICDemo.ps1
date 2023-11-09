0cc6259f85c52fee4ca2e6f301cfa93e854f0621
Jan Vidar Elven <jan.vidar.elven@evidi.com>
2023-11-09 10:32:31 +0100

    Update AA runbook

--- IdentityAutomation/AzureAutomationPowerShell/vscodeAutomation/995179128/GetGraphUserNICDemo.ps1
+++ IdentityAutomation/AzureAutomationPowerShell/vscodeAutomation/995179128/GetGraphUserNICDemo.ps1
@@ -1,6 +1,14 @@
-
 # Connect to Microsoft Graph using Managed Identity
-Connect-MgGraph -Identity
+# This should work, but got errors in 7.2 preview runtime
+# Using AzAccount workaround instead
+# Connect-MgGraph -Identity
+
+# Connect to Az using Managed Identity
+Connect-AzAccount -Identity
+
+$accessToken = Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com"
+$secureToken = ConvertTo-SecureString -String $accessToken.Token -AsPlainText -Force
+Connect-MgGraph -AccessToken $secureToken
 
 # Perform desired operations
 # For example, get a list of users
